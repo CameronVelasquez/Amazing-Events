a7fec328fdff0e63f761bc3d46f1db2e2cb28523
mypla <cameronlino@outlook.com>
2022-12-29 10:55:41 -0300

    cards from js

---
+++ assets/home.js
@@ -0,0 +1,31 @@
+
+
+let where = document.getElementById("cards-section")
+
+ function crearCards( lista, va ){
+     let todasLasCards = ""
+     for (let recorrido of lista){
+        let template =
+                    `<div class="card" style="width: 18rem;">
+                    <figure id="cardImg">
+                    <img src="${recorrido.image}" class="card-img-top" alt="${recorrido.name}">
+                    </figure>
+                    <div class="card-body">
+                    <h5 class="card-title">${recorrido.name}</h5>
+                    <p class="card-text">${recorrido.description}</p>
+                    </div>
+                    <div class="card-end">
+                    <p>Price: ${recorrido.price}</p>
+                    <a href="details.html" class="btn">Ver Más</a>
+                    </div>
+                    </div>`
+    todasLasCards += template            
+        
+}
+console.log(va)
+  va.innerHTML = todasLasCards
+}
+crearCards (data.events, where)
+
+
+
